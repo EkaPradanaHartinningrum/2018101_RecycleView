3b4d4e45533414b219e6550a7a36b31046218209
EkaPradanahartinningrum <eka4833@gmail.com>
2022-11-01 20:53:02 +0700

    2018101 Recycle View

---
+++ app/src/main/java/com/example/tugas_recycleview/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.tugas_recycleview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recylerView;
+    String s1[], s2[],s3[];
+    int images[] =
+            {R.drawable.mortar1,R.drawable.plesterplus2,R.drawable.thinbed3,R.drawable.portland4,R.drawable.portland5,R.drawable.semenputih6};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recylerView = findViewById(R.id.recyclerView);
+        s1 = getResources().getStringArray(R.array.makanan);
+        s2 = getResources().getStringArray(R.array.deskripsi);
+        s3 = getResources().getStringArray(R.array.star);
+        SemenAdapter appAdapter = new
+                SemenAdapter(this,s1,s2,s3,images);
+        recylerView.setAdapter(appAdapter);
+        recylerView.setLayoutManager(new
+                LinearLayoutManager(this));
+    }
+}
